80ab6750acc1825652a1b2a50f2fa683d4e300b3
Yixuan Wei <weason1998@gmail.com>
2021-04-15 16:43:50 +0800

    Update generate_meta.py

--- generate_meta.py
+++ generate_meta.py
@@ -7,19 +7,19 @@ from tqdm import tqdm
 import json
 import argparse
 parser = argparse.ArgumentParser('Format json')
-parser.add_argument('--split_idx', type=str, help='split_idx should in odds')
+parser.add_argument('--split_idx', type=int, help='split_idx should in odds')
 args = parser.parse_args()
 
-caption_file = []
 
-def cap_gen(args, df, caption_file):
+def cap_gen(args, df):
+    caption_file = []
     tsv_file = 'cc12_split_{}.tsv'.format(args.split_idx)
     caption_map = {}
     with open(tsv_file) as f:
         infos = f.read().splitlines()
         for i in infos:
             i = i.split('\t')
-            cpation_map[i[0]] = i[1]
+            caption_map[i[0]] = i[-1]
 
     for item in tqdm(df.iterrows()):
         type = item[1]['mimetype']
@@ -27,16 +27,21 @@ def cap_gen(args, df, caption_file):
             ext = type.split('/')[-1]
             if ext.lower() in ['jpeg', 'png', 'jpg']:
                 img_idx = item[1]['file'].split('/')[-1]
-                caption_file.append({'caption': caption_map[img_idx], 'image': 'image_split_{}.zip@/{}.{}'.format(args.split_idx, img_idx, ext)})
+                try:
+                    caption_file.append({'caption': caption_map['{:08d}'.format(int(img_idx))], 'image': 'image_split_{}.zip@/{}.{}'.format(args.split_idx, img_idx, ext)})
+                except:
+                    print(item[1])
+                    break
     return caption_file
 
 df = pd.read_csv('./downloaded_training_report.tsv.gz', compression='gzip', sep='\t', names=["file","folder","headers","mimetype","size","status","url"])
 print(pd.DataFrame(df.groupby('mimetype')['url'].count()).reset_index(level=0).sort_values(by='url', ascending=False))
-caption_file = cap_gen(args, df, caption_file)
+caption_file = cap_gen(args, df)
+json.dump(caption_file, open('caption_train_{}.json'.format(args.split_idx), 'w'))
 
 df = pd.read_csv('./downloaded_training_report.tsv.gz.part0', compression='gzip', sep='\t', names=["file","folder","headers","mimetype","size","status","url"])
 print(pd.DataFrame(df.groupby('mimetype')['url'].count()).reset_index(level=0).sort_values(by='url', ascending=False))
 args.split_idx = args.split_idx-1
-caption_file = cap_gen(args, df, caption_file)
+caption_file = cap_gen(args, df)
 
-json.dump(caption_file, open('caption_train.json', 'w'))
+json.dump(caption_file, open('caption_train_{}.json'.format(args.split_idx), 'w'))
